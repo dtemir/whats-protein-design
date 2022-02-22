44904d5355322b876390e0fa89ab5ebcc4cfd827
Damir Temir <dtemi2@uis.edu>
2022-02-22 14:32:53 -0600

    Final presentation

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'docs')))
 
 app.get('*', (_, res) =>
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'))
 );
 
 app.listen(port, () => console.log(`Ready on port ${port}`));
